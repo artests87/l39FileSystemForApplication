4637d2da4c70e7bc89c232dddac227ad21783456
artests <illusors1>
2016-02-04 15:40:14 +0300

    S

---
+++ app/src/main/java/com/example/artests/l39filesystemforapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.artests.l39filesystemforapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
